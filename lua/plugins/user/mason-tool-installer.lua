0e4be10fcb869d4eafa7d705764449e12240b38e
Fasca-kcct <r121317@g.kobe-kosen.ac.jp>
2023-06-20 22:58:32 +0900

    first commit

---
+++ lua/plugins/user/mason-tool-installer.lua
@@ -0,0 +1,40 @@
+local M = {}
+function M.setup()
+  return {
+    ensure_installed = {
+      -- Lsp
+      -- "clangd",
+      "pyright",
+      "lua-language-server",
+      "typescript-language-server",
+      "rust-analyzer",
+      "vim-language-server",
+      "html-lsp",
+      "css-lsp",
+      "json-lsp",
+      "emmet-ls",
+
+      -- Formatter
+      "prettierd",
+      -- "prettier",
+      "stylua",
+      "black",
+      "yamlfmt",
+
+      -- Linter
+      "mypy",
+      "hadolint",
+      "eslint_d",
+      -- "eslint-lsp",
+
+      -- Diagnostics
+      "cspell",
+
+      -- Dap
+      -- "debugpy",
+    },
+  }
+end
+
+return M
+
